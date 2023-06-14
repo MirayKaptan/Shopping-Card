4bcd8a6edd48e4a858a3f6ebb1c4bc982df1dbd2
miray.kaptan <mrykptn@gmail.com>
2023-06-14 23:46:26 +0300

    Faker API added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,15 @@
 import React from "react";
+import { faker } from "@faker-js/faker";
 
 const Home = () => {
+  const productArray = [...Array(20)].map(() => ({
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.image.dataUri(),
+  }));
+
+  console.log(productArray);
+
   return <div>Home</div>;
 };
 
