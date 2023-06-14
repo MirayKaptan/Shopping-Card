d0a24b8bfef45d17feab2eb59c4d50051f003d1b
miray.kaptan <mrykptn@gmail.com>
2023-06-14 19:55:10 +0300

    Components added.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/card" element={<Card />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
