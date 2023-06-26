7c751d8a0352033ae01779505696d25a314aa5b6
Miray Kaptan <107173011+MirayKaptan@users.noreply.github.com>
2023-06-26 14:56:34 +0300

    products and basket changed

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
 
-const Basket = () => {
-  return <div>Card</div>;
+const Basket = ({card, setCard}) => {
+  const [total, setTotal] = useState();
+  useEffect(()=>{
+    setTotal(card.reduce((acc, curr)=> acc  + Number(curr.price),0))
+  },[card])
+  return <div>
+    <span>My Card</span>
+    <br/>
+    <span>Total: {total}</span>
+    <div> {
+      card.map(prod => (
+
+        <Product prod={prod} key={prod.id} card={card} setCard={setCard}/>
+      ))
+      
+      
+      } </div>
+  </div>;
 };
 
 export default Basket;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,14 +1,31 @@
 import React from "react";
 
-const Product = ({ prod }) => {
+const Product = ({ prod, card, setCard }) => {
   return (
-    <div className="flex flex-col m-3 w-[25%]">
+    <div className="flex flex-col m-3 w-[25%] p-3 border border-sky-500 ">
       <img src={prod.image} alt={prod.name} />
       <div className="flex truncate">{prod.name}</div>
       <div>{prod.price} TL </div>
-      <button class="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded">
-        Add to Card
-      </button>
+
+      {card.includes(prod) ? (
+        <button
+          className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            setCard(card.filter((c) => c.id !== prod.id));
+          }}
+        >
+          Remove the Card
+        </button>
+      ) : (
+        <button
+          className="bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
+          onClick={() => {
+            setCard([...card, prod]);
+          }}
+        >
+          Add to Card
+        </button>
+      )}
     </div>
   );
 };
