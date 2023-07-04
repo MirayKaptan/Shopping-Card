b784183cdaa8e0fd480b11e6e97b3f847aee2b94
Miray Kaptan <107173011+MirayKaptan@users.noreply.github.com>
2023-07-04 16:41:17 +0300

    Context added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Basket from "./components/Basket";
 
 function App() {
-  const[card, setCard] = useState([])
+
   return (
     <Router>
       <div className="App">
-        <Header card={card} setCard={setCard}/>
+        <Header/>
         <Routes>
-          <Route path="/" element={<Home card={card} setCard={setCard}/>} />
-          <Route path="/basket" element={<Basket card={card} setCard={setCard}/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/basket" element={<Basket/>} />
         </Routes>
       </div>
     </Router>
